a11ddfc414c6a04220b6ca565b841d2a0616179a
Lorenzo Benetollo <lorenzo.benetollo@gmail.com>
2022-12-24 23:38:22 +0100

    tasks module replaced by matches module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MatchesModule } from './matches/matches.module';
 import configuration from './config';
 
 @Module({
@@ -23,7 +23,7 @@ import configuration from './config';
     ScheduleModule.forRoot(),
     AuthModule,
     UsersModule,
-    TasksModule,
+    MatchesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
